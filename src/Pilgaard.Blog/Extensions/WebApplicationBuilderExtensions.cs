f77e7a58314090a2acb56b6fafa9bc4428e0e7a8
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-10-02 22:20:42 +0200

    removing a bunch of stuff we don't need

--- src/Pilgaard.Blog/Extensions/WebApplicationBuilderExtensions.cs
+++ src/Pilgaard.Blog/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,20 +1,24 @@
-using Pilgaard.Blog.Data;
-
 namespace Pilgaard.Blog.Extensions;
 
 public static class WebApplicationBuilderExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder, string[] args)
     {
-        builder.Services.AddSignalR().AddAzureSignalR(options =>
+        if (!builder.Environment.IsDevelopment())
         {
-            options.ServerStickyMode =
-                Microsoft.Azure.SignalR.ServerStickyMode.Required;
-        });
+            builder.Services.AddSignalR().AddAzureSignalR(options =>
+                    {
+                        options.ServerStickyMode =
+                            Microsoft.Azure.SignalR.ServerStickyMode.Required;
+                    });
+        }
+        else
+        {
+            builder.Services.AddSignalR();
+        }
 
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
-        builder.Services.AddSingleton<WeatherForecastService>();
 
         return builder.Build();
     }
--- tests/Pilgaard.Blog.Tests/UnitTest1.cs
+++
@@ -1,1 +0,0 @@
-namespace Pilgaard.Blog.Tests;
