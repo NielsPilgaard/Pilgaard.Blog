a34c3df72f7529b0aec5ff96a3d221466be37b49
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-10-14 22:30:00 +0200

    moving things around again, it's a bit messy to not have a database it seems

---
+++ src/Pilgaard.Blog/Data/BlogPosts.cs
@@ -0,0 +1,17 @@
+﻿using Pilgaard.Blog.Models;
+
+namespace Pilgaard.Blog.Data;
+
+public static class BlogPosts
+{
+    public static readonly BlogPostSeries MakingABlog = new("Making a Blog", "making-a-blog", 1, new BlogPost[]
+    {
+        new("Planning", "1-planning", new DateOnly(2022, 10, 9), 1, "planning"),
+        new("Hosting on Azure", "2-hosting",new DateOnly(2022, 10, 9),  2, "hosting", "azure"),
+        new("Building the MVP", "3-building-the-mvp", new DateOnly(2022, 10, 9), 3, "prismjs", "seo"),
+        new("Improving Appearances", "4-improving-appearances", new DateOnly(2022, 10, 14), 4, "mudblazor", "css"),
+
+    }, "blazor");
+
+    public static readonly BlogPostSeries[] AllBlogPostSeries = { MakingABlog };
+}
