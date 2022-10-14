d141e42ce01592ff351e6a478d9c4656966aaa89
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-10-14 22:20:33 +0200

    changed the format I "store" blog posts in

---
+++ src/Pilgaard.Blog/Models/BlogPost.cs
@@ -0,0 +1,8 @@
+﻿namespace Pilgaard.Blog.Models;
+
+public readonly record struct BlogPost(
+    string DisplayName,
+    string PathName,
+    DateOnly PublishDate,
+    int NumberInSeries = 1,
+    params string[] Tags);
---
+++ src/Pilgaard.Blog/Models/BlogPostSeries.cs
@@ -0,0 +1,15 @@
+﻿namespace Pilgaard.Blog.Models
+{
+    public record BlogPostSeries(
+        string DisplayName,
+        string PathName,
+        int Order,
+        BlogPost[] BlogPosts,
+        params string[] Tags)
+    {
+        public string GetRelativePath(BlogPost blogPost)
+        {
+            return "posts/" + PathName + "/" + blogPost.PathName;
+        }
+    }
+}
