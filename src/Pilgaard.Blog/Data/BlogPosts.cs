33ba647407c15feac1cc348eaae49b927b12a451
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-11-02 19:23:07 +0100

    updated models in preperation for using a database

--- src/Pilgaard.Blog/Data/BlogPosts.cs
+++ src/Pilgaard.Blog/Data/BlogPosts.cs
@@ -4,15 +4,66 @@ namespace Pilgaard.Blog.Data;
 
 public static class BlogPosts
 {
-    public static readonly BlogPostSeries MakingABlog = new("Making a Blog with C#", "making-a-blog", 1, new BlogPost[]
-    {
-        new("Planning", "1-planning", new DateOnly(2022, 10, 9), 1, "planning"),
-        new("Hosting on Azure", "2-hosting",new DateOnly(2022, 10, 9),  2, "hosting", "azure"),
-        new("Building the MVP", "3-building-the-mvp", new DateOnly(2022, 10, 9), 3, "prismjs", "seo"),
-        new("Improving Appearances", "4-improving-appearances", new DateOnly(2022, 10, 19), 4, "mudblazor", "css", "html"),
-        new("Adding Comments", "5-adding-comments", new DateOnly(2022, 10, 22), 5, "giscus", "github"),
-
-    }, "blazor");
+    public static readonly BlogPostSeries MakingABlog = new(
+        title: "Making a Blog with C#",
+        description: "In this series, we build *this* website from scratch, using NET6.0 and blazor",
+        pathName: "making-a-blog",
+        order: 1,
+        blogPosts: new BlogPost[]
+        {
+            new(title: "Planning",
+                description: "In this post, we layout the plan for making a blog.",
+                pathName: "1-planning",
+                publishDate: new DateOnly(year: 2022, month: 10, day: 9),
+                numberInSeries: 1,
+                "planning"),
+            new(title: "Hosting on Azure",
+                description: "In this post, we look at how to host Blazor Server, and where to host it.",
+                pathName: "2-hosting",
+                publishDate: new DateOnly(year: 2022, month: 10, day: 9),
+                numberInSeries: 2,
+                "hosting", "azure"),
+            new(title: "Building the MVP",
+                description: "In this post, we build the blog MVP, and go live!",
+                pathName: "3-building-the-mvp",
+                publishDate: new DateOnly(year: 2022, month: 10, day: 9),
+                numberInSeries: 3,
+                "prismjs",
+                "seo"),
+            new(title: "Improving Appearances",
+                description: "In this post, we make the blog nicer to look at. I hope.",
+                pathName: "4-improving-appearances",
+                publishDate: new DateOnly(year: 2022, month: 10, day: 19),
+                numberInSeries: 4,
+                "mudblazor",
+                "css",
+                "html"),
+            new(title: "Adding Comments",
+                description: "In this post, we add a comment section to each blog post.",
+                pathName: "5-adding-comments",
+                publishDate: new DateOnly(year: 2022,
+                    month: 10,
+                    day: 22),
+                numberInSeries: 5,
+                "giscus",
+                "github"),
+            new(title: "SEO",
+                description: "In this post, we add Search Engine Optimizations to make the blog easier to find.",
+                pathName: "6-seo",
+                publishDate: new DateOnly(year: 2022,
+                    month: 10,
+                    day: 22),
+                numberInSeries: 6,
+                "seo"),
+            //new (title: "",
+            //    description: "",
+            //    pathName: "",
+            //    publishDate: new DateOnly(2022,
+            //        10,
+            //        22),
+            //    numberInSeries: 6,
+            //    ""),
+        }, "blazor", "dotnet");
 
     public static readonly BlogPostSeries[] AllBlogPostSeries = { MakingABlog };
 }
--- src/Pilgaard.Blog/Models/BlogPost.cs
+++ src/Pilgaard.Blog/Models/BlogPost.cs
@@ -1,8 +1,26 @@
 ﻿namespace Pilgaard.Blog.Models;
 
-public readonly record struct BlogPost(
-    string DisplayName,
-    string PathName,
-    DateOnly PublishDate,
-    int NumberInSeries = 1,
-    params string[] Tags);
+public class BlogPost
+{
+    public BlogPost(string title,
+        string description,
+        string pathName,
+        DateOnly publishDate,
+        int numberInSeries = 1,
+        params string[] tags)
+    {
+        Title = title;
+        Description = description;
+        PathName = pathName;
+        PublishDate = publishDate;
+        NumberInSeries = numberInSeries;
+        Tags = tags;
+    }
+
+    public string Title { get; }
+    public string Description { get; }
+    public string PathName { get; }
+    public DateOnly PublishDate { get; }
+    public int NumberInSeries { get; }
+    public string[] Tags { get; }
+}
